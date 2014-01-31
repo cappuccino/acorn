6ffbc5084cda157df9b75b3af2d2f5c019c55575
Aparajita Fishman <aparajita@aparajita.com>
2014-01-31 12:30:28 +0100

    Add multiline example with control structure.

--- test/preprocessor.js
+++ test/preprocessor.js
@@ -30,6 +30,14 @@ x = /* before */ COMMENTS(13) /* after */;
 x = [NUMBERS];
 // x = [1, 2, 3];
 
+#define ROUND(v, n) \
+if (n - Math.floor(n) >= 0.5) \
+    v = Math.floor(n); \
+else \
+    v = Math.ceil(n);
+
+ROUND(x, 13.27);
+
 // Macros only take effect from point of definition
 foo = X;
 // foo = X;
