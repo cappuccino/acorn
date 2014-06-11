c743a1305fd22e5214668a5a3a740edbdf5d69ee
Aparajita Fishman <aparajita@aparajita.com>
2014-06-11 13:34:15 -0400

    Don't complain about reserved words in property name position
    Also add forbidReserved: "everywhere" support.
    
    See https://github.com/marijnh/acorn/commit/5bd50cce6f29f76c0c6bc09a46b8782a0dca8fd9

--- acorn.js
+++ acorn.js
@@ -80,7 +80,9 @@
     // trailing commas in array and object literals.
     allowTrailingCommas: true,
     // By default, reserved words are not enforced. Enable
-    // `forbidReserved` to enforce them.
+    // `forbidReserved` to enforce them. When this option has the
+    // value "everywhere", reserved words and keywords can also not be
+    // used as property names.
     forbidReserved: false,
     // When `trackComments` is turned on, the parser will attach
     // `commentsBefore` and `commentsAfter` properties to AST nodes
@@ -1291,10 +1293,6 @@
       }
       if (isKeyword(word)) type = keywordTypes[word];
       else if (options.objj && isKeywordObjJ(word)) type = keywordTypesObjJ[word];
-      else if (options.forbidReserved &&
-               (options.ecmaVersion === 3 ? isReservedWord3 : isReservedWord5)(word) ||
-               strict && isStrictReservedWord(word))
-        raise(tokStart, "The keyword '" + word + "' is reserved");
     }
     return finishToken(type, word);
   }
@@ -4136,7 +4134,20 @@
 
   function parseIdent(liberal) {
     var node = startNode();
-    node.name = tokType === _name ? tokVal : (((liberal && !options.forbidReserved) || tokType.okAsIdent) && tokType.keyword) || unexpected();
+    if (liberal && options.forbidReserved == "everywhere") liberal = false;
+    if (tokType === _name) {
+      if (!liberal &&
+          (options.forbidReserved &&
+           (options.ecmaVersion === 3 ? isReservedWord3 : isReservedWord5)(tokVal) ||
+           strict && isStrictReservedWord(tokVal)) &&
+          input.slice(tokStart, tokEnd).indexOf("\\") == -1)
+        raise(tokStart, "The keyword '" + tokVal + "' is reserved");
+      node.name = tokVal;
+    } else if (liberal && tokType.keyword) {
+      node.name = tokType.keyword;
+    } else {
+      unexpected();
+    }
     tokRegexpAllowed = false;
     next();
     return finishNode(node, "Identifier");
